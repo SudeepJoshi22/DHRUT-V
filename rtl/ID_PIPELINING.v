f9549b6167d92520ce96e23bb169b434157b0a6a
ShreyasGK3204 <shreyasgk.ec21@rvce.edu.in>
2024-05-29 19:25:39 +0530

    Pipelined

--- rtl/ID_PIPELINING.v
+++ rtl/ID_PIPELINING.v
@@ -34,13 +34,21 @@ input wire [31:0] i_instr, // Instruction Fetched in IF stage
 input wire signed [31:0] i_write_data, // Data to be written to register file from the WB stage
 input wire [31:0] i_pc, // PC of the current instruction
 input wire i_wr, // write enable signal from the WB stage, enables the register file to write to rd.
-output wire [31:0] o_rs1_data, // rs1 data from register file
-output wire [31:0] o_rs2_data, // rs2 data from register file
-output wire [31:0] o_imm_data, // signa extended immediate value
-output wire [6:0] o_opcode, // opcode of the current instruction
-output wire [2:0] o_func3, // func3 of the current instruction
-output wire [3:0] o_alu_ctrl, // ALU Control signals  
-output wire [31:0] o_pc // PC for the next stage
+input wire i_ce,
+output reg [31:0] o_rs1_data, // rs1 data from register file
+output reg [31:0] o_rs2_data, // rs2 data from register file
+output reg [31:0] o_imm_data, // signa extended immediate value
+output reg [6:0] o_opcode, // opcode of the current instruction
+output reg [2:0] o_func3, // func3 of the current instruction
+output reg [3:0] o_alu_ctrl, // ALU Control signals  
+output reg [31:0] o_pc,// PC for the next stage
+output reg branch_flush, // Flush the IF stage
+output reg branch_pc, 
+// pipeline control
+input wire i_stall, // stall signal from EX stage 
+output reg o_stall, // stall signal to IF stage
+output reg o_flush, // Flush signal to IF depending on branch decision
+output reg o_ce
 );
 
 wire [4:0] is_rs1, is_rs2, is_rd;
@@ -48,41 +56,62 @@ wire [6:0] is_opcode;
 wire [2:0] is_func3;
 wire is_re;
 wire is_alu_ctrl;
-
-assign is_rs1 = i_instr[19:15];
-assign is_rs2 = i_instr[24:20];
-assign is_rd = i_instr[11:7];
-assign is_opcode = i_instr[6:0];
-assign is_func3 = i_instr[14:12];
-
-assign o_func3 = is_func3;
-assign o_opcode = is_opcode;
-
+wire [31:0] is_rs1_data;
+wire [31:0] is_rs2_data; 
+wire [31:0] is_imm;
+wire is_boj;
+reg  [31:0] d_isntr; // used for decoding
+d_instr= (is_boj==1)?NOP:i_instr; // flush this stage if is_boj==1
+// Decode of instructions
+assign is_rs1 = d_instr[19:15];
+assign is_rs2 = d_instr[24:20];
+assign is_rd = d_instr[11:7];
+assign is_opcode = d_instr[6:0];
+assign is_func3 = d_instr[14:12];
 assign is_re = ~((is_opcode == `J) | (is_opcode == `U) | (is_opcode == `UPC)); // every instruction except LUI, AUIPC and JAL requires register file to be read
 
-assign o_pc = i_pc;
+always@(posedge clk, negedge rst_n)
+begin
+	if(rst_n) begin
+	o_ce<=0;
+	end
+	else if( i_ce && !(is_boj||i_stall) )begin // pipe through signals when stage is enabled and not stalled
+	o_rs1_data<=is_rs1_data;
+	o_rs2_data<=is_rs2_data;
+	o_imm_data<=is_imm;
+	o_opcode<=is_opcode;
+	o_func3<=is_func3;
+	o_alu_ctrl<=is_alu_ctrl;
+	end
+end
+	
+	
+	
+
+
 
 reg_file reg_file_inst(
 	.clk(clk),
 	.rst_n(rst_n),
 	.i_re(is_re),
 	.i_wr(i_wr),
-	.i_rs1(is_rs1),
-	.i_rs2(is_rs2),
+	.i_rs1(is_rs1_data),
+	.i_rs2(is_rs2_data),
 	.i_rd(is_rd),
 	.i_write_data(i_write_data),
-	.o_read_data1(o_rs1_data),
-	.o_read_data2(o_rs2_data)
+	.o_read_data1(is_rs1_data),
+	.o_read_data2(is_rs2_data)
 );
 
 imm_gen imm_gen_inst (
   .i_instr(i_instr),
-  .o_imm_data(o_imm_data)
+  .o_imm_data(is_imm)
 );
 
 control_unit control_unit_inst (
-  .i_instr(i_instr),
+  .i_instr(d_instr),
   .o_alu_ctrl(is_alu_ctrl)
 );
+
 
 endmodule
