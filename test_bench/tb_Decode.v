94052dde9becb4a3d78800123b73f69aa8665c31
ShreyasGK3204 <shreyasgk.ec21@rvce.edu.in>
2024-07-03 15:22:00 +0530

    Removed clock gating, internal flush is made sensitive to clock

--- test_bench/tb_Decode.v
+++ test_bench/tb_Decode.v
@@ -8,7 +8,7 @@ module tb_Decode();
   reg [31:0] i_write_data;
   reg [31:0] i_pc;
   reg i_wr;
-  reg i_ce;
+  //reg i_ce;
   wire [31:0] o_rs1_data;
   wire [31:0] o_rs2_data;
   wire [31:0] o_imm_data;
@@ -17,10 +17,11 @@ module tb_Decode();
   wire [3:0] o_alu_ctrl;
   wire [31:0] o_pc;
   wire  [31:0] branch_pc;
+  reg i_prediction;
   reg i_stall; 
   wire o_stall; 
   wire o_flush; 
-  wire o_ce;
+  //wire o_ce;
 
   // Instantiate the module
   ID inst(
@@ -30,7 +31,7 @@ module tb_Decode();
     .i_write_data(i_write_data),
     .i_pc(i_pc),
     .i_wr(i_wr),
-    .i_ce(i_ce),
+    //.i_ce(i_ce),
     .o_rs1_data(o_rs1_data),
     .o_rs2_data(o_rs2_data),
     .o_imm_data(o_imm_data),
@@ -38,10 +39,11 @@ module tb_Decode();
     .o_func3(o_func3),
     .o_alu_ctrl(o_alu_ctrl),
     .branch_pc(branch_pc),
+    .i_prediction(i_prediction),
     .i_stall(i_stall),
     .o_stall(o_stall),
-    .o_flush(o_flush),
-    .o_ce(o_ce)
+    .o_flush(o_flush)
+    //.o_ce(o_ce)
   );
 
   // Clock generation
@@ -64,43 +66,29 @@ module tb_Decode();
     i_write_data = 32'h0;
     i_pc = 32'h0;
     i_wr = 0;
-    i_ce=1;
+    i_prediction = 1;
     i_stall=0;    
-    #20
-    i_instr= 32'h00840393; //addi x7,x5,8
+    #30
+    i_instr=32'h00218333; //add x6,x3,x2
     #20
     i_instr=32'h00431663; // bne x6,x4,12
-    #10
-    
-  
+    #20
+    i_instr= 32'h00840393;//addi x7,x5,8
+    #20
+    i_instr=32'h00828467; //jalr x8,8(x5)
+    #20
+    i_instr=32'h0000C36F; //jal x6 , 12
     #200;
     $finish;
   end
-      
-
-    //#10; // Wait for a few clock cycles before toggling reset
-
-    //rst_n = 1;
-    //#20; // Provide some time after releasing reset
-
-    // Test cases with instructions
-  //  i_ce=1;
-  //  i_instr=32'h00218333; //add x6,x3,x2
-  // #10
-  //  i_ce=1;
-  // i_instr=32'h18431663; // bne x6,x4,12
-  // #10
-    //i_ce=1;
-  // i_instr=32'h00840393; //addi x7,x5,8
-  // #10
-    //i_ce=1;
-  // i_instr=32'h00828467; //jalr x8,8(x5)
-    // Add more test cases if needed
+       // Add more test cases if needed
 
     // Finish the simulation
 
 
   // Add stimulus or other test bench code if needed
+
+   
 
 endmodule
 
